3916b0087ebc8d6418bd6272f3db6f2ce58a013c
Walacy Batista <walacybatistapires19@gmail.com>
2024-03-29 02:06:54 -0300

    Criando update e read

--- index.ts
+++ index.ts
@@ -11,27 +11,48 @@ app.get('/', (req: Request, res: Response) => {
     return res.status(200).json('Minha primeira rota');
 });
 
-// Rota para criar um novo produto
 app.post('/products', async (req: Request, res: Response) => {
     const { name } = req.body;
     try {
       const produtos = await prisma.produtos.create({ data: { nome: name } });
-      return res.status(201).json(produtos);
+      return res.status(200).json(produtos);
     } catch (error) {
-      return res.status(500).json({ error: 'Erro ao criar produto.' });
+      return res.status(400).json({ error: 'Erro ao criar produto.' });
     }
   });
 
-// Rota para deletar um novo produto
 app.delete('/products/:id', async (req: Request, res: Response) => {
     const {id} = req.params;
     try {
       await prisma.produtos.delete({ where: { id: parseInt(id) } });
-      return res.status(204).send();
+      return res.status(200).send();
     } catch (error) {
-      return res.status(500).json({ error: 'Erro ao excluir produto.' });
+      return res.status(400).json({ error: 'Erro ao excluir produto.' });
     }
   });
+
+app.put('/products/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { nome } = req.body;
+    try {
+      const UpdateProduto = await prisma.produtos.update({
+        where: { id: parseInt(id) },
+        data: { nome }
+      });
+      return res.status(200).json(UpdateProduto);
+    } catch (error) {
+      return res.status(400).json({ error: 'Erro ao atualizar produto.' });
+    }
+});
+
+app.get('/products', async (req: Request, res: Response) => {
+    try {
+      const ReadProduto = await prisma.produtos.findMany();
+      return res.status(200).json(ReadProduto);
+    } catch (error) {
+      return res.status(400).json({ error: 'Erro ao buscar produtos.' });
+    }
+});
 
 const PORT = process.env.PORT;
 
